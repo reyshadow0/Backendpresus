e4203d64f3c579a348ca4aea65160a07f4c26bfb
reyshadow0 <162193242+reyshadow0@users.noreply.github.com>
2026-03-16 14:12:37 -0500

    Delete backend/backend-mod/backend-output directory
    Eliminacion

--- backend/backend-mod/backend-output/src/main/java/ec/edu/uteq/presustentaciones/repositories/SalaRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.uteq.presustentaciones.repositories;
-
-import ec.edu.uteq.presustentaciones.entities.Sala;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalaRepository extends JpaRepository<Sala, Long> {
-}
